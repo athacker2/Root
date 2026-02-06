f3d1109afcfab48dd2366732752a18f06babfb0d
Abhay Thacker <abhaythacker2@gmail.com>
2026-02-06 15:04:30 -0800

    made all classes in types into dataclasses and moved board creation logic into separate setup file

--- src/rootgame/engine/types.py
+++ src/rootgame/engine/types.py
@@ -1,41 +1,21 @@
-class GameState:
-    def __init__(self):
-        self.players = []
-        self.board = None
-        self.turn = 0
+from dataclasses import dataclass
 
+@dataclass
 class Player:
-    def __init__(self):
-        self.score = 0
+    score: int = 0
 
-
-# Board/Map related classes
-class Board:
-    def __init__(self):
-        self.clearings = []
-    
-    def initialize_board(self):
-        # Initialize the board with clearings and connections
-        self.clearings = [Clearing() for _ in range(12)]
-
-        self.clearings[0].adjacentClearings = [self.clearings[4], self.clearings[3], self.clearings[1]]
-        self.clearings[1].adjacentClearings = [self.clearings[0], self.clearings[2]]
-        self.clearings[2].adjacentClearings = [self.clearings[1], self.clearings[3], self.clearings[7]]
-        self.clearings[3].adjacentClearings = [self.clearings[0], self.clearings[5], self.clearings[2]]
-        self.clearings[4].adjacentClearings = [self.clearings[0], self.clearings[8], self.clearings[5]]
-        self.clearings[5].adjacentClearings = [self.clearings[4], self.clearings[8], self.clearings[10], self.clearings[6], self.clearings[3]]
-        self.clearings[6].adjacentClearings = [self.clearings[5], self.clearings[11], self.clearings[7]]
-        self.clearings[7].adjacentClearings = [self.clearings[2],self.clearings[6], self.clearings[11]]
-        self.clearings[8].adjacentClearings = [self.clearings[4], self.clearings[5], self.clearings[9]]
-        self.clearings[9].adjacentClearings = [self.clearings[8], self.clearings[10]]
-        self.clearings[10].adjacentClearings = [self.cleartings[9], self.clearings[5], self.clearings[11]]
-        self.clearings[11].adjacentClearings = [self.clearings[6], self.clearings[7], self.clearings[10]]
-            
-
-
-
+@dataclass
 class Clearing:
-    def __init__(self):
-        self.adjacentClearings = []
-        self.tiles = []
-        self.suit = []
+    adjacentClearings: list[int]
+    tiles: list[str]
+    suit: str | None
+
+@dataclass
+class Board:
+    clearings: list[Clearing]
+
+@dataclass
+class GameState:
+    players: list[Player]
+    board: Board | None
+    turn: int = 0

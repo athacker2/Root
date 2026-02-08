23d820741ae1f0af2d61c7649a45b6cf89672047
Abhay Thacker <abhaythacker2@gmail.com>
2026-02-08 08:58:32 -0800

    added character field to players

--- src/rootgame/engine/player.py
+++ src/rootgame/engine/player.py
@@ -1,6 +1,16 @@
 from dataclasses import dataclass, field
+from enum import Enum
+
+class Character(Enum):
+    NONE = 0
+    MARQUISE_DE_CAT = 1
+    EYRIE_DYNASTIES = 2
+    WOODLAND_ALLIANCE = 3
+    VAGABOND = 4
+
 
 @dataclass
 class Player:
     score: int = 0
     hand: list = field(default_factory=list)
+    character: Character = Character.NONE
